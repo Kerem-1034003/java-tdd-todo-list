52ea6dc0a7a5535bb502d8d19d60a2d6f147e717
Your Name <kerem-1034003>
2025-08-07 15:56:30 +0200

    core requirements finished Task class in task.java

--- src/main/java/com/booleanuk/core/Task.java
+++ src/main/java/com/booleanuk/core/Task.java
@@ -19,4 +19,5 @@ public class Task {
         this.completed = completed;
     }
 
+
 }
--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -2,6 +2,7 @@ package com.booleanuk.core;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Comparator;
 
 public class TodoList {
     private List<Task> tasks;
@@ -33,5 +34,59 @@ public class TodoList {
         }
         return false;
     }
+
+    public List<Task> getCompleteTasks() {
+        List<Task> completed = new ArrayList<>();
+        for (Task task : this.tasks) {
+            if (task.isCompleted()) {
+                completed.add(task);
+            }
+        }
+        return completed;
+    }
+
+    public List<Task> getIncompleteTasks(){
+        List<Task> incompleted = new ArrayList<>();
+        for (Task task : this.tasks) {
+            if (!task.isCompleted()){
+                incompleted.add(task);
+            }
+        }
+        return incompleted;
+    }
+
+    public String findTask(String name) {
+        for (Task task : tasks) {
+            if (task.getName().equals(name)) {
+                return task.getName();
+            }
+
+        }
+        return "Task not found!";
+    }
+
+    public boolean removeTask(String name) {
+        for (Task task : tasks) {
+            if (task.getName().equals(name)) {
+                tasks.remove(task);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Task> getTasksSortedAscending() {
+        List<Task> sortedTasks = new ArrayList<>(tasks);
+        sortedTasks.sort(Comparator.comparing(Task::getName));
+        return sortedTasks;
+    }
+
+    public List<Task> getTasksSortedDescending() {
+        List<Task> sortedTasks = new ArrayList<>(tasks);
+        sortedTasks.sort(Comparator.comparing(Task::getName).reversed());
+        return sortedTasks;
+    }
+
+
 
 }
